df2e453e3eb645f44b66a5495e2b30ab76f71b77
Timothee AUPETIT <timothee.aupetit@proton.me>
2024-02-26 15:54:12 +0100

    Fixed computeLength

--- src/main/scala/org/perso/TLVManager.scala
+++ src/main/scala/org/perso/TLVManager.scala
@@ -38,12 +38,12 @@ object TLVManager extends App {
   }
 
   def computeLength(bytes: ByteVector): ByteVector = {
-    val inter1 = bytes & hex"00ff" // AND
+    val inter1 = bytes & hex"ff00" // AND
     val inter2 = inter1 & hex"7fff" // AND
-    val inter3 = inter1 ^ inter2 // XOR
-    val mask = inter3 >> 1 // shift 1 bit
+    val inter3 = inter2 >> 1 // shift 1 bit to the right
+    val mask = inter3 ^ inter1 // XOR
 
-    mask ^ inter1 // XOR
+    mask ^ bytes // XOR
   }
 
   def lengthToLong(l: ByteVector): Long = computeLength(l).toLong()
